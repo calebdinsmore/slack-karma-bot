907c8374fe750b6b8a8a94fa6d6c48ee78f6b33b
Caleb Dinsmore <calebjdinsmore@gmail.com>
2017-07-30 17:08:36 -0400

    Readded print bot id script

---
+++ print_bot_id.py
@@ -0,0 +1,16 @@
+import os
+from slackclient import SlackClient
+
+BOT_NAME = "karma-bot"
+
+slackclient = SlackClient(os.environ.get('KARMA_BOT_TOKEN'))
+
+if __name__ == "__main__":
+    api_call = slackclient.api_call("users.list")
+    if api_call.get('ok'):
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print("Bot ID for '" + user['name'] + "' is " + user['id'])
+    else:
+        print("Could not find user with the name " + BOT_NAME)
